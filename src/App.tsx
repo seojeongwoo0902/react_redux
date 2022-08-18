2831ca87de4c3bd742933bce9884686ec6dca9be
seojengwoo0902 <realteapots@vaultmicro.com>
2022-08-18 16:26:29 +0900

    second

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,44 @@
-import React from 'react';
-import styled from "styled-components";
-import './App.css';
 import { createStore } from "redux";
+const form = document.querySelector("form");
+const input = document.querySelector("input");
+const ul = document.querySelector("ul");
 
-const StyledButton = styled.button`
-  padding: 6px 12px;
-  border-radius: 8px;
-  font-size: 1rem;
-  line-height: 1.5;
-  border: 1px solid lightgray;
-  color: gray;
-  background: white;
-`;
-
+const ADD_TODO = "ADD_TODO"
+const DELETE_TODO = "DELETE_TODO"
 
 
 function App() {
-  const countModifier = (count = 5, action: any): number => {
-    if (action.type === "ADD") {
-      return count + 1
+  const reducer = (state = [], action: any): any => {
+    switch (action.type) {
+      case ADD_TODO:
+        return [...state, { text: action.text }]
+      case DELETE_TODO:
+        return []
+      default:
+        return state
     }
-    else if (action.type === "MINUS") {
-      return count - 1
-    }
-    else { return count }
   }
-  const countStore = createStore<number, any, unknown, unknown>(countModifier)
-  const number: any = document.querySelector("span");
-  number.innerText = 0;
-  const onChange = () => {
-    if (number != undefined) {
-      number.innerText = countStore.getState()
-    }
+  const store = createStore(reducer)
 
-    console.log(countStore.getState())
+  store.subscribe(() => {
+    console.log(store.getState())
+  })
+
+  const onSubmit = (e: any) => {
+    e.preventDefault()
+    const toDo = input?.value;
+    input!.value = ""
+    store.dispatch({ type: ADD_TODO, text: toDo })
   }
-  countStore.subscribe(onChange)
-
-  const add = () => { countStore.dispatch({ type: "ADD" }) }
-  const minus = () => { countStore.dispatch({ type: "MINUS" }) }
-
-
+  form?.addEventListener("submit", onSubmit)
   return (
     <div className="App">
-      <StyledButton onClick={add}>Add</StyledButton>
-      <span></span>
-      <StyledButton onClick={minus}>Minus</StyledButton>
+      <h1>To Dos</h1>
+      <form>
+        <input type="text" placeholder="Write to do" />
+        <button>Add</button>
+      </form>
+      <ul></ul>
     </div>
   );
 }
